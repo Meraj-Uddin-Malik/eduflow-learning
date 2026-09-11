55d00228de608d3a110fe5b6165fe29945bddec3
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2026-09-11 21:55:29 +0500

    Update student object to set feePaid to false and add age property

--- 01-javascript-refresh/03-objects.js
+++ 01-javascript-refresh/03-objects.js
@@ -7,5 +7,6 @@ const student = {
 // console.log(student) // Output: { stdName: 'John Doe', className: '10th Grade', feePaid: true }
 // console.log({stdName: student.stdName, className: student.className, feePaid: student.feePaid }) // Output: { stdName: 'John Doe', className: '10th Grade', feePaid: true }
 
-student.feePaid = false
+student.feePaid = false;
+student.age = 15;
 console.log(student) // Output: { stdName: 'John Doe', className: '10th Grade', feePaid: false }
