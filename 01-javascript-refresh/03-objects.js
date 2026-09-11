c7636f942b1675623f8049d8b306031ba05fbc1d
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2026-09-11 22:09:17 +0500

    Add bracket notation examples for accessing student object properties

--- 01-javascript-refresh/03-objects.js
+++ 01-javascript-refresh/03-objects.js
@@ -12,3 +12,11 @@ student.age = 15;
 
 delete student.age;
 console.log(student) // Output: { stdName: 'John Doe', className: '10th Grade', feePaid: false }
+
+// Bracket notation:
+
+console.log(student["stdName"]) // Output: John Doe
+
+const key = "className";
+console.log(student[key]) // Output: 10th Grade
+
